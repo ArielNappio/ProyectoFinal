32f7ec559735d522766355bfd0974a2d3465cd43
Andy <72051588+andygoeshard@users.noreply.github.com>
2025-04-18 13:39:47 -0300

    test2
    coso

--- app/src/main/java/com/example/proyectofinal/presentation/view/Home.kt
+++ app/src/main/java/com/example/proyectofinal/presentation/view/Home.kt
@@ -1,4 +1,4 @@
-package com.example.proyectofinal.presentation
+package com.example.proyectofinal.presentation.view
 
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -30,7 +29,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
