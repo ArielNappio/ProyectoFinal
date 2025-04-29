b213664825364e4b23e873923ce7c576272e22ef
iamrochi <rmercado373@alumno.unlam.edu.ar>
2025-04-29 16:37:58 -0300

    cambios en el cu de UpdateOrderUseCase

--- app/src/main/java/com/example/proyectofinal/domain/usecase/UpdateOrderUseCase.kt
+++ app/src/main/java/com/example/proyectofinal/domain/usecase/UpdateOrderUseCase.kt
@@ -4,5 +4,5 @@ import com.example.proyectofinal.data.remoteData.model.Order
 import com.example.proyectofinal.data.remoteData.repository.RemoteRepository
 
 class UpdateOrderUseCase(private val remoteRepository: RemoteRepository) {
-    operator fun invoke(order: Order) = remoteRepository.updateOrder(order.id, order)
+    operator fun invoke(order: Order) = remoteRepository.updateOrder(order)
 }
