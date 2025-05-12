5a610b0bc1779a1036b24d7b3e11f4858694821f
iamrochi <rmercado373@alumno.unlam.edu.ar>
2025-05-12 10:38:27 -0300

    en el vm de la home, se cambiaron el contenido de la lista

--- app/src/main/java/com/example/proyectofinal/student/presentation/viewmodel/HomeScreenViewModel.kt
+++ app/src/main/java/com/example/proyectofinal/student/presentation/viewmodel/HomeScreenViewModel.kt
@@ -26,13 +26,14 @@ class HomeScreenViewModel : ViewModel() {
 
     private fun loadNotes() {
         _notes.value = listOf(
-            Note(1, "Nota 1", "Descripción de la nota 1", false),
-            Note(2, "Nota 2", "Descripción de la nota 2", true),
-            Note(3, "Nota 3", "Descripción de la nota 3", false),
-            Note(4, "Nota 1", "Descripción de la nota 1", false),
-            Note(5, "Nota 2", "Descripción de la nota 2", true),
-            Note(6, "Nota 3", "Descripción de la nota 3", false),
-            Note(6, "Nota 3", "Descripción de la nota 3", false)
+            Note(1, "Apunte de Redes", "Resumen de los modelos OSI y TCP/IP con ejemplos.", false),
+            Note(2, "Práctica de Estadística", "Ejercicios de media, mediana, moda y desvío estándar.", true),
+            Note(3, "Parcial de Base de Datos", "Preguntas resueltas del parcial de SQL y modelado.", true),
+            Note(4, "Trabajo Práctico de UI/UX", "Prototipo y diseño de app para gestión de turnos médicos.", false),
+            Note(5, "Resumen de Historia Económica", "Principales crisis económicas del siglo XX.", true),
+            Note(6, "Apunte de Arquitectura de Computadoras", "Resumen de tipos de memoria y ciclos de instrucción.", false),
+            Note(7, "Filosofía: clase 3", "Ideas principales de Platón y su teoría de las Ideas.", false),
         )
     }
+
 }
