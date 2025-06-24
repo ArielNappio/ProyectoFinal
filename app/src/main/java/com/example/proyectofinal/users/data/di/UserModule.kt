1c6fd79d3b4d5536e565f4ada5d93e27d93799b4
tomasaranda <tomas.aranda@despegar.com>
2025-06-24 20:27:07 -0300

    fix users di

--- app/src/main/java/com/example/proyectofinal/users/data/di/UserModule.kt
+++ app/src/main/java/com/example/proyectofinal/users/data/di/UserModule.kt
@@ -1,31 +1,27 @@
 package com.example.proyectofinal.users.data.di
 
 import UpdateUserUseCase
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.proyectofinal.users.data.provider.UserProviderImpl
 import com.example.proyectofinal.users.domain.provider.UserProvider
 import com.example.proyectofinal.users.domain.provider.usecase.CreateUserUseCase
 import com.example.proyectofinal.users.domain.provider.usecase.DeleteUserUseCase
 import com.example.proyectofinal.users.domain.provider.usecase.GetUserUseCase
 import com.example.proyectofinal.users.presentation.viewmodel.UserViewModel
-import io.ktor.client.HttpClient
-import org.koin.dsl.module
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val userModule = module {
 
-    single  { UserProviderImpl(get()) } // El get() aquí obtiene el HttpClient
+    single<UserProvider> { UserProviderImpl(get()) }
 
-        viewModel {
+    viewModel {
         UserViewModel(
             getUserUserCase = get(),
             deleteUserUseCase = get(),
-            updateUserUseCase = get() ,
+            updateUserUseCase = get(),
             createUserUseCase = get()
         )
     }
-
 
     factory { GetUserUseCase(get()) }
     factory { DeleteUserUseCase(get()) }
