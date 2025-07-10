bc94977878c0c7f1f103c50178360e0d3d511949
Andy <72051588+andygoeshard@users.noreply.github.com>
2025-07-10 14:28:25 -0300

    ui update detailscreen

--- app/src/main/java/com/example/proyectofinal/student/presentation/view/ProjectDetailScreen.kt
+++ app/src/main/java/com/example/proyectofinal/student/presentation/view/ProjectDetailScreen.kt
@@ -1,7 +1,6 @@
 package com.example.proyectofinal.student.presentation.view
 
 import LoadingWithImageBar
-import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -24,6 +23,7 @@ import androidx.compose.material.icons.filled.Description
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -32,7 +32,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -70,10 +69,10 @@ fun ProjectDetailScreen(
 
     Scaffold(
         topBar = {
-            TopAppBar(
+            CenterAlignedTopAppBar(
                 title = {
                     AppText(
-                        text = "Detalles del Proyecto",
+                        text = "Detalles",
                         isTitle = true
                     )
                 },
